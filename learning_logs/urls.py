d07a18061bfe39b01c60994385cbb687f0089ebb
Matthew Sweere <matthewsweere@Matthews-MacBook-Pro-3.local>
2020-05-01 16:22:39 -0500

    More Video Work

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,13 +11,18 @@ app_name = 'learning_logs'
 
 # The variable urlpatterns in this module is a list of individual pages
 # that can be requested from the learning_logs app
-urlpatterns = {
+urlpatterns = [
     # the first argument is an empty string (' ') which matches the
     # base URL - http://localhost:8000/. The second argument specifies 
     # the function name to call in views.py. The third argument provides
     # the name 'index' for this URL pattern to refer to it later
     path('', views.index, name = 'index'),
-    #path('topics', views.topics, name = 'topics')
+    path('topics', views.topics, name = 'topics'),
+    # the integer value is stored in the variable topic_id and will
+    # be subsequently passed to the topic function in views.py
+    path('topics/<int:topic_id>/', views.topic, name = 'topic'),
+    path('new_topic/', views.new_topic, name = 'new_topic'),
+    path('new_entry/<int:topic_id>/', views.new_entry, name = 'new_entry'),
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name = 'edit_entry')
 
-
-}
+]
